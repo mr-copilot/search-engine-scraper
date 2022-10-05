89bc79434c5ae897fa7ec2d55b591c96cfa30fa5
mr-copilot <infoamir225@gmail.com>
2022-10-05 19:54:48 +0530

    done promoted related changes

--- src/Adapter/Process.php
+++ src/Adapter/Process.php
@@ -25,8 +25,10 @@ class Process implements \AdapterInterface
              MS\ErrorException::ADAPTER_CURL_UNABLE_INIT_CURL
          );
       }
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
       curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
       curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
       curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, false);
       curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
@@ -39,7 +41,6 @@ class Process implements \AdapterInterface
               MS\ErrorException::ADAPTER_CURL_EXECUTE_FAILED
           );
       }
-
       return $response;
 
   }
@@ -49,6 +50,7 @@ class Process implements \AdapterInterface
       // TODO: Implement parseHTMLtoArray() method.
       $dom = new \DOMDocument();
       @$dom->loadHTML($html);
+      @//$dom->loadHTMLFile($html);
       $links = $dom->getElementsByTagName('a');
 
       $results = [];
@@ -56,20 +58,44 @@ class Process implements \AdapterInterface
       foreach ($links as $link){
 
           $item = $link->getAttribute('href');
-          $url_pattern = '/url?q=';
+          $url_pattern = '/aclk?';
           if(strpos($item, $url_pattern) !== false){
 
+              if (!filter_var($item, FILTER_VALIDATE_URL) === false) {
+                  $contentArr = array_values(array_filter(explode('  ', $link->parentNode->parentNode->textContent),'trim'));
+                  $contentArrNew = [];
+                  $j = 0;
+                  foreach ($contentArr as $contentItem) {
+                      if (strlen($contentItem) > 20) { // can be analyzed further
+                          $contentArrNew[$j] = $contentItem;
+                          $j++;
+                      }
+                  }
+
+                  if(!empty($contentArrNew[0]) && strlen($contentArrNew[0])<75 && !empty($contentArrNew[1])){
+                      $results[$i]['keyword'] = $keyword;
+                      $results[$i]['url'] = $item;
+                      $results[$i]['title'] = str_replace('...','', $contentArr[0]);
+                      $results[$i]['desc'] = str_replace('...','', $contentArr[1]);
+                      $results[$i]['rank'] = $i+1;
+                      $results[$i]['promoted'] = 1;
+                      $i++;
+                  }
+              }
+          }
+          $url_pattern = '/url?q=';
+          if(strpos($item, $url_pattern) !== false){
               $item = str_ireplace($url_pattern, '', $item);
               $url = strtok($item,'&');
               if (!filter_var($url, FILTER_VALIDATE_URL) === false && strpos($url,$engine) === false) {
-                  $contentArr = array_values(array_filter(explode('  ', $link->parentNode->parentNode->textContent)));
-                  //print_r($contentArr);
+                  $contentArr = array_values(array_filter(explode('  ', $link->parentNode->parentNode->textContent), 'trim'));
                   if(!empty($contentArr[0]) && strlen($contentArr[0])<75 && !empty($contentArr[2])){
                       $results[$i]['keyword'] = $keyword;
                       $results[$i]['url'] = $url;
                       $results[$i]['title'] = str_replace('...','', $contentArr[0]);
                       $results[$i]['desc'] = str_replace('...','', $contentArr[2]);
                       $results[$i]['rank'] = $i+1;
+                      $results[$i]['promoted'] = 0;
                       $i++;
                   }
               }
